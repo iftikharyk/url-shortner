0954f1b9d06b0cd3af727f6241baf1b5ae169813
Iftikhar Yar Khan <34522152+iftikharyk@users.noreply.github.com>
2020-09-04 17:39:20 +0500

    create new url on local mongo working, yayyyy!

--- server/index.js
+++ server/index.js
@@ -2,9 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const yup = require('yup');
+const monk = require('monk');
+const { nanoid } = require('nanoid');
+require('dotenv').config();
+
+const db = monk(process.env.MONGO_URI);
+const urls = db.get('urls');
+urls.createIndex('name');
 
 const app = express();
 const port = process.env.PORT || 1337;
+
+const schema = yup.object().shape({
+    slug: yup.string().trim().matches(/[\w\-]/i),
+    url: yup.string().url().required()
+});
 
 app.use(helmet());
 app.use(morgan('tiny'));
@@ -22,9 +35,53 @@ app.use(express.static('./public'));
 //     // redirects to url
 // });
 
-// app.post('/url', (req, res) => {
-//     // create new url
-// });
+app.post('/url', async (req, res, next) => {
+    // create new url
+    let { slug, url } = req.body;
+
+    try {
+        if (!slug) {
+            slug = nanoid(5);
+        } else {
+            const urlExist = await urls.findOne({ slug });
+            if (urlExist) {
+                throw new Error('Slug is in use');
+            }
+        }
+
+        slug = slug.toLowerCase();
+        // const secret = nanoid(10).toLowerCase();
+
+        await schema.validate({
+            slug,
+            url
+        });
+
+        const newUrl = {
+            url,
+            slug
+        }
+        const created = await urls.insert(newUrl);
+
+        res.json({ created });
+
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.use((error, req, res, next) => {
+    if (error.status) {
+        res.status(error.status);
+    } else {
+        res.status(500);
+    }
+
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? 'Do not worry, everything will be alright soon!' : error.stack
+    });
+});
 
 // app.get('/url/:id', (req, res) => {
 //     // get url detail
