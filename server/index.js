4ba5ae2b8a7294c6d8d0b571d960de00a748ecdf
Iftikhar Yar Khan <34522152+iftikharyk@users.noreply.github.com>
2020-09-04 15:07:10 +0500

    static template added

--- server/index.js
+++ server/index.js
@@ -10,12 +10,25 @@ app.use(helmet());
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
+app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
-    res.json({
-        message: "Welcome to the world of nothingness"
-    });
-});
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: "Welcome to the world of nothingness"
+//     });
+// });
+
+// app.get('/:id', (req, res) => {
+//     // redirects to url
+// });
+
+// app.post('/url', (req, res) => {
+//     // create new url
+// });
+
+// app.get('/url/:id', (req, res) => {
+//     // get url detail
+// });
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
